fa1441ae2c3d244546c4f1c3bde28aed9da65f48
Brant Ramey <brantramey@Brants-MacBook-Pro.local>
2025-03-26 00:05:32 -0500

    Updated Navbar to include mobile devices

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,32 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar: React.FC = () => {
+  const [click, setClick] = useState(false);
+  
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
           Christ Centered Baptist
         </Link>
-        <ul className="nav-menu">
+        
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
           <li className="nav-item">
-            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/" className="nav-link" onClick={closeMobileMenu}>Home</Link>
           </li>
           <li className="nav-item">
-            <Link to="/salvation" className="nav-link">Salvation</Link>
+            <Link to="/salvation" className="nav-link" onClick={closeMobileMenu}>Salvation</Link>
           </li>
           <li className="nav-item">
-            <Link to="/hymns" className="nav-link">Hymns</Link>
+            <Link to="/hymns" className="nav-link" onClick={closeMobileMenu}>Hymns</Link>
           </li>
           <li className="nav-item">
-            <Link to="/armor-of-god" className="nav-link">Armor of God</Link>
+            <Link to="/armor-of-god" className="nav-link" onClick={closeMobileMenu}>Armor of God</Link>
           </li>
           <li className="nav-item">
-            <Link to="/jesus-is-god" className="nav-link">Jesus is God</Link>
+            <Link to="/jesus-is-god" className="nav-link" onClick={closeMobileMenu}>Jesus is God</Link>
           </li>
           <li className="nav-item">
-            <Link to="/about" className="nav-link">About</Link>
+            <Link to="/about" className="nav-link" onClick={closeMobileMenu}>About</Link>
           </li>
         </ul>
       </div>
